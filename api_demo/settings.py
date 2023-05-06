a5285d0be3b75c8b29909c362d23996d69bf03f6
wicked-smart <marzaanvan@gmail.com>
2023-05-06 21:33:22 +0530

    Modify ALLOWED_HOSTS

--- api_demo/settings.py
+++ api_demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5xcm6v_-4%z@yfkh&5*($v(7rr-8em#yawavh&((xh7u$--91^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
