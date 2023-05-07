65bfbb4b6738b9a4a37c3c84cab8a341eaf4efa4
wicked-smart <marzaanvan@gmail.com>
2023-05-07 18:44:42 +0530

    modify .gitignore files and re-add files

--- api_demo/settings.py
+++ api_demo/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dotenv import load_dotenv 
+
+load_env = load_dotenv('.env')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5xcm6v_-4%z@yfkh&5*($v(7rr-8em#yawavh&((xh7u$--91^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
